c60570df4f2653c3492dde398f4c0b136d222b63
SidraMoosa <sidramoosa94@gmail.com>
2024-09-12 01:45:58 -0700

    add files

---
+++ app.js
@@ -0,0 +1,242 @@
+        //Chapter 6 (Math Expression II)
+ 
+// q 1. 
+//  x= x++;        
+//  x= ++x;
+
+//q 2.
+// var num= 100;
+// var number= num--;
+// console.log(num);
+
+//q 3.
+// var x= 50;
+// var y= x++;
+// console.log(x);
+
+//q 4.
+// var y= 50;
+// var z = --y;
+// console.log(y);
+
+//q 5.
+//  var num= 10;
+//  var newNum= num--;
+//  console.log(num);
+
+//q 6.
+// var num=20;
+// var anotherNumber= num++;
+// console.log(num);
+
+//q 7.
+// var num = 30;
+// number= num++;
+// alert(num);
+
+            //Chapter 7 (Math Expression III)
+//q 1.
+// var calculateNum= 2 +(2*6);            
+// console.log(calculateNum);
+
+//q 2.
+// var calculatedNum = (2 + 2) * 6;
+// console.log(calculatedNum);
+
+//q 3.
+// var calculatedNum = (2 + 2) * (4 + 2);
+// console.log(calculatedNum);
+
+//q 4.
+// var calculatedNum = ((2 + 2) * 4) + 2;
+// console.log(calculatedNum);
+
+//q 5.
+// var cost=(2 + 2) * (4 + 10);
+// console.log(cost);
+
+//q 6.
+// var cost= 2+ (2*4)+10;
+// console.log(cost);
+
+//q 7.
+// var pressure= (4/2)*4;
+// console.log(pressure);
+
+
+                //Chapter 8 (Concatenating Text Strings)
+//q 1.
+// var num = "2" +"2";
+// console.log(num);
+
+//q 2.
+// var message = "Hello," + "Dolly";
+// alert(message);
+
+//q 3.
+//alert("33" + 3);
+   
+//q 4.
+// var num1= "Pakistan";
+// var num2="Zindabad";
+// console.log(num1+" "+ num2);
+
+
+//q 5.
+// var result = "My age is " + 20;
+// console.log(result);
+
+//q 6.
+// var firstName= "Sidra";
+// var lastName= "Moosa";
+// var fullName = firstName+" "+ lastName;
+// console.log(fullName);
+
+
+                //Chapter 9 (Prompts)
+//q 1.                
+// var firstName = prompt("Enter first name");
+// console.log(firstName)
+
+//q 2.
+// var country = prompt("Country?", "China");
+// console.log(country);
+
+//q 3.
+// var yourName = prompt("Enter Your Name");
+// console.log(yourName);
+
+//q 4.
+// var userAnswer = prompt("Please enter your qualifiaction:", "Intermediate");
+// console.log(userAnswer);
+
+//q 5.
+// var message = "Please enter your favorite color:";
+// var defaultResponse = "Blue";
+// var userResponse = prompt(message, defaultResponse);
+// console.log(userResponse);
+
+//q 6.
+// var messageDefult1 = prompt("Please enter your fullName:", "SidraMoosa");
+// var messageDefult2 = prompt("Please enter your qualifiaction:", "Intermediate");
+// var userResponse = messageDefult1+" "+ messageDefult2;
+// alert(userResponse);
+
+                //Chapter 5 pdf MATH EXPRESSIONS
+
+//q 1.                
+// var num1 = 3;
+// var num2 = 5;
+// var sum = num1+num2
+// document.write("Sum of"+" "+ num1+" "+ "and"+" "+ num2+" "+ "is"+" "+ sum );
+
+//q 2.
+// var num1 = 3;
+// var num2 = 5;
+// var sum = num1-num2
+// document.write("Sum of"+" "+ num1+" "+ "and"+" "+ num2+" "+ "is"+" "+ sum );
+
+// var num1 = 3;
+// var num2 = 5;
+// var sum = num1*num2
+// document.write("Sum of"+" "+ num1+" "+ "and"+" "+ num2+" "+ "is"+" "+ sum );
+
+// var num1 = 3;
+// var num2 = 5;
+// var sum = num1/num2
+// document.write("Sum of"+" "+ num1+" "+ "and"+" "+ num2+" "+ "is"+" "+ sum );
+
+// var num1 = 3;
+// var num2 = 5;
+// var sum = num1%num2
+//document.write("Sum of"+" "+ num1+" "+ "and"+" "+ num2+" "+ "is"+" "+ sum );
+
+
+//q 3.
+// a
+// var myVariable;
+
+// b.
+//  var num= "“Value after variable declaration is: ??”.";
+//  document.write(num);
+
+//c.
+// var myVariable=5;
+// console.log(myVariable);
+
+//d.
+// var variable= "“Initial value: 5”."
+//  document.write(variable);
+
+//e.
+// var num=5;
+// var result=num++;
+// console.log(num);
+
+//f.
+// var value="“Value after increment is: 6”."
+// document.write(value);
+
+//g.
+// var num=6;
+// var popularNum= num+7;
+// console.log(popularNum);
+
+//h.
+// var value="“Value after addition is: 13”."
+// document.write(value);
+
+//i.
+// var num=13;
+// var result=num--;
+// console.log(num);
+
+//j.
+// var value="“Value after decrement is: 12”."
+//  document.write(value);
+
+//k.
+// var num1 = 3;
+// var num2 = 3;
+// console.log(num1*num2);
+
+//l.
+// var value="“The remainder is : 0”.";
+//  document.write(value);
+
+
+//q 4.
+// var price=600;
+// var tickets= 5;
+// var totalCost= price* tickets;
+// document.write("Total cost to buy"+" "+ tickets+" "+ "Tickets to amovie is" +" "+totalCost+" "+"PKR");
+
+//q 6.
+// var Celsius=25;
+// var Fahrenheit=(Celsius*9/5)+32;
+// document.write("25oC is"+" "+  Fahrenheit+"oF");
+
+// var Fahrenheit=70;
+// var Celsius=(Fahrenheit-32)*9/5;
+// document.write("70oC is"+" "+  Celsius+"oF");
+
+//q 7.
+// var priceItem1 =650;
+// var quantityItem1 =3;
+// var priceItem2 = 100; 
+// var quantityItem2 =7;
+// var shippingCharges =100;
+// var totalCost= priceItem1*quantityItem1*quantityItem2*priceItem2*shippingCharges;
+
+// document.write("Price of item 1 is"+" "+priceItem1+" "+  "quantity of item 1 is"+" "+quantityItem1+" "+ "Price of item 2 is"+" "+priceItem2+
+//  "Quantity of item 2 is"+""+ quantityItem2+" "+ "Shipping charges"+" "+shippingCharges);
+
+//  document.write("Total cost pof your order is"+" "+totalCost);
+
+//q 8.
+// var totalMarks= 980;
+// var markObtained= 804;
+// var percentage=(totalMarks/markObtained)*100;
+// document.write("Toatl mark"+" "+totalMarks+" " +"Mark obtained"+" "+markObtained+" "+percentage);
+
+//q 9.
